89f4f939f8c96a244cc1f08e82f5456d5c5b87c5
Emilio Ambrosio <emilio.ambrosio@joyn.de>
2023-03-31 18:33:19 +0200

    Add the first web-framework agnostic integration test

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-
 // We need to mark `run` as public.
-// It is no longer a binary entrypoint, therefore we can mark it as async 
+// It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro.
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+
+// Notice the different signature!
+// We return `Server` on the happy path and we dropped the `async` keyword 
+// We have no .await call, so it is not needed anymore.
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .run();
+    //no .await here
+    Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,35 @@
-use newsletter::main;
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
 
-#[test]
-fn dummy_test() {
-    main()
+    // We need to bring in `reqwest`
+    // to perform HTTP requests against our application.
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// No .await call, therefore no need for `spawn_app` to be async now.
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can just panic and crash
+// all the things.
+async fn spawn_app() {
+    let server = newsletter::run().expect("Failed to bind address");
+    // Launch the server as a background task
+
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
 }
